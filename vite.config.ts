d28d4ce6f88d4f28d6bf3110c1ff915aa69a0893
Varelion <Business.8508@gmail.com>
2025-06-17 12:19:21 -0400

    Add swc

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svgr(), react()],
+    plugins: [
+        svgr(),
+        react({
+            plugins: [
+                // You can add SWC plugins here if needed
+            ],
+        }),
+    ],
+    build: {
+        reportCompressedSize: false, // This can speed up builds
+    },
+    // Your existing optimizeDeps config
     optimizeDeps: {
-        //workaround for the problem https://github.com/vitejs/vite/issues/7719
         extensions: ['.css'],
         esbuildOptions: {
             plugins: [
